37a0950465b77146e9de513471b7f88c928b487f
Jiayi Zhu <jiayi@fb.com>
2015-04-23 04:04:06 -0700

    adding previous page functionality

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-// The MIT License (MIT)
-//
-// Copyright (c) 2015 pangpangzhu
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
 var express = require('express');
 var async = require('async');
 var Browser = require('zombie');
@@ -28,23 +6,24 @@ var http = require('http');
 
 var router = express.Router();
 var browser = new Browser();
-var current_img = null;
-var button = null;
 
 browser.userAgent = 'Mozilla/5.0 (Linux; U; en-US) AppleWebKit/528.5+ (KHTML, like Gecko, Safari/528.5+) Version/4.0 Kindle/3.0 (screen 600×800; rotate)';
 
 // This is the page on youyaoqi from where you want to start reading.
-var starting_url = 'http://www.u17.com/chapter/342351.html';
+var starting_url = 'http://www.u17.com/chapter/351497.html';
 
 var check_page = function (callback) {
   return function() {
-    this.current_img =
+    var new_img =
       browser.document.getElementById('current_read_image')
         .getElementsByClassName('cur_img')[0];
-    this.button =
+    this.button_next =
       browser.document.getElementsByClassName('pagebar')[0]
         .getElementsByClassName('next')[0];
-    return callback(this.current_img, this.button);
+    this.button_prev =
+      browser.document.getElementsByClassName('pagebar')[0]
+        .getElementsByClassName('prev')[0];
+    return callback(new_img);
   }
 };
 
@@ -53,10 +32,13 @@ async.series([
     browser.visit(
       starting_url,
       check_page(
-        function(img, button) {
-          return
-            typeof img != 'undefined' &&
-            typeof button != 'undefined';
+        function(img) {
+          if (typeof img !== 'undefined') {
+            this.current_img = img;
+            this.all_imgs = [];
+            return true;
+          }
+          return false;
         }
       ),
       function () {
@@ -68,16 +50,28 @@ async.series([
 ]);
 
 router.get('/', function (req, res) {
+  if (typeof req.query.button == 'undefined') {
+    req.query.button = 'next';
+  }
+
+  if (req.query.button == 'next') {
+    if (this.prev_img) {
+      this.all_imgs.push(this.prev_img);
+    }
+    this.prev_img = this.current_img.src;
+  } else {
+    this.prev_img = this.all_imgs.pop();
+  }
+
   res.render(
     'index',
-    {
-      imgsrc: this.current_img.src,
-    }
+    { imgsrc: req.query.button == 'prev' ? this.prev_img : this.current_img.src }
   );
+
   async.series([
     function (done) {
       browser.click(
-        this.button,
+        req.query.button == 'prev' ? this.button_prev : this.button_next,
         function (e) {
           if (e) { console.log('[browser error] ' + e); }
           done();
@@ -88,7 +82,11 @@ router.get('/', function (req, res) {
       browser.wait(
         check_page(
           function (img) {
-            return img.className.indexOf('cur_img') == -1;
+            if (img && img.src != this.current_img.src) {
+              this.current_img = img;
+              return true;
+            }
+            return false;
           }
         ),
         function (e) {
